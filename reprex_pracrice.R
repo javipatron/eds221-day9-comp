0e91e9eb993b6f7965edadbb163b88e1656b1b6a
JaviPatron <jpatronmf@gmail.com>
2022-08-18 15:45:38 -0700

    Initial commit

--- reprex_pracrice.R
+++ reprex_pracrice.R
@@ -1,2 +1,33 @@
 # ---------------Practicing reprex ------------------#
 
+library(tidyverse)
+library(palmerpenguins)
+
+
+penguins |>
+  select(species, body_mass_g, flipper_length_mm, year) |>
+  filter(species == "Chinstrap") |>
+  str_to_lower(species) |>
+  group_by(island) |>
+  summarize(mean(body_mass_g, na.rm = TRUE),
+            mean(flipper_length_mm, na.rm =TRUE))
+
+
+### A REPREX
+library(tidyverse)
+
+warpbreaks |> 
+  str_to_lower(wool)
+
+
+### THE FIXING CODE
+# Mutating the new column
+
+warpbreak |> 
+  mutate(wool = str_to_lower(wool))
+
+
+
+
+
+
